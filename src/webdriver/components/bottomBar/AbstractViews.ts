846e67e2cfb1fc56f915abaefa17abe395428379
Jan Richter <jrichter@redhat.com>
2019-05-15 15:06:33 +0200

    Add current channel retrieval to bottom views

--- src/webdriver/components/bottomBar/AbstractViews.ts
+++ src/webdriver/components/bottomBar/AbstractViews.ts
@@ -1,4 +1,4 @@
-import { By, until, Key } from 'selenium-webdriver';
+import { By, until, Key, WebElement } from 'selenium-webdriver';
 import * as clipboard from 'clipboardy';
 import { ElementWithContexMenu } from "../ElementWithContextMenu";
 
@@ -12,44 +12,65 @@ export abstract class ChannelView extends ElementWithContexMenu {
     * Get names of all selectable channels
     * @returns array of strings - channel names
     */
-   async getChannelNames(): Promise<string[]> {
-       const names: string[] = [];
-       const elements = await this.enclosingItem.findElement(By.xpath(`.//ul[@aria-label='${this.actionsLabel}']`))
-           .findElements(By.tagName('option'));
-       
-       for (const element of elements) {
-           const disabled = await element.getAttribute('disabled');
-           if (!disabled) {
-               names.push(await element.getAttribute('value'));
-           }
-       }
-       return names;
-   }
+    async getChannelNames(): Promise<string[]> {
+        const names: string[] = [];
+        const elements = await this.enclosingItem.findElement(By.xpath(`.//ul[@aria-label='${this.actionsLabel}']`))
+            .findElements(By.tagName('option'));
 
-   /**
-    * Select a channel using the selector combo
-    * @param name name of the channel to open
-    */
-   async selectChannel(name: string): Promise<void> {
-       const combo = await this.enclosingItem.findElement(By.tagName('select'));
-       const workbench = await this.getDriver().findElement(By.className('monaco-workbench'));
-       const menu = await workbench.findElement(By.className('context-view'));
+        for (const element of elements) {
+            const disabled = await element.getAttribute('disabled');
+            if (!disabled) {
+                names.push(await element.getAttribute('value'));
+            }
+        }
+        return names;
+    }
 
-       if (await menu.isDisplayed()) {
-           await combo.click();
-           await this.getDriver().wait(until.elementIsNotVisible(menu));
-       }
-       await combo.click();
-       const rows = await menu.findElements(By.className('monaco-list-row'));
-       for (let i = 0; i < rows.length; i++) {
-           if ((await rows[i].getAttribute('class')).indexOf('disabled') < 0) {
-               const text = await rows[i].findElement(By.className('option-text')).getText();
-               if (name === text) {
-                   return await rows[i].click();
-               }
-           }
-       }
-   }
+    /**
+     * Get name of the current channel
+     */
+    async getCurrentChannel(): Promise<string> {
+        let text!: string;
+        const combo = await this.enclosingItem.findElement(By.tagName('select'));
+        const rows = await this.getOptions();
+        for (const row of rows) {
+            if ((await row.getAttribute('class')).indexOf('focused') > -1) {
+                text = await row.findElement(By.className('option-text')).getText();
+                break;
+            }
+        }
+        await combo.click();
+        return text;
+    }
+
+    /**
+     * Select a channel using the selector combo
+     * @param name name of the channel to open
+     */
+    async selectChannel(name: string): Promise<void> {
+        const rows = await this.getOptions();
+        for (let i = 0; i < rows.length; i++) {
+            if ((await rows[i].getAttribute('class')).indexOf('disabled') < 0) {
+                const text = await rows[i].findElement(By.className('option-text')).getText();
+                if (name === text) {
+                    return await rows[i].click();
+                }
+            }
+        }
+    }
+
+    private async getOptions(): Promise<WebElement[]> {
+        const combo = await this.enclosingItem.findElement(By.tagName('select'));
+        const workbench = await this.getDriver().findElement(By.className('monaco-workbench'));
+        const menu = await workbench.findElement(By.className('context-view'));
+
+        if (await menu.isDisplayed()) {
+            await combo.click();
+            await this.getDriver().wait(until.elementIsNotVisible(menu));
+        }
+        await combo.click();
+        return await menu.findElements(By.className('monaco-list-row'));
+    }
 }
 
 /**
