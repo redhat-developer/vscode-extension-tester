1c148fe5cfb1049c334449a6088a0adfe1c5b10a
Jan Richter <jrichter@redhat.com>
2019-05-29 11:53:10 +0200

    Bring back the initial keypress for open dialog

--- src/webdriver/native/openDialog.ts
+++ src/webdriver/native/openDialog.ts
@@ -23,6 +23,7 @@ export class LinuxOpenDialog implements OpenDialog {
         if (!fs.existsSync(absolutePath)) {
             throw new Error('The selected path does not exist');
         }
+        await robot.sendKey('up');
         await robot.sendCombination(['control', 'l']);
         await new Promise((res) => { setTimeout(res, 1000); });
         await robot.sendText(absolutePath);
