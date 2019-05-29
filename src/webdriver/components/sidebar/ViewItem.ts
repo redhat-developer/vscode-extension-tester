5dc66e6e2e3d3955c56605360cc0827853b763cc
Jan Richter <jrichter@redhat.com>
2019-05-29 10:40:17 +0200

    Add action buttons to view item

--- src/webdriver/components/sidebar/ViewItem.ts
+++ src/webdriver/components/sidebar/ViewItem.ts
@@ -1,4 +1,6 @@
 import { ElementWithContexMenu } from "../ElementWithContextMenu";
+import { AbstractElement } from "../AbstractElement";
+import { By, WebElement } from "selenium-webdriver";
 
 /**
  * Page object representing a row in the tree inside a view content section
@@ -54,5 +56,62 @@ export abstract class ViewItem extends ElementWithContexMenu {
         if (await this.hasChildren() && await this.isExpanded()) {
             await this.click();
         }
+    }
+
+    /**
+     * Find all action buttons bound to the view item
+     * 
+     * @returns array of ViewItemAction objects, empty array if item has no
+     * actions associated
+     */
+    async getActionButtons(): Promise<ViewItemAction[]> {
+        let container: WebElement;
+        try {
+            container = await this.findElement(By.className('actions-container'));
+        } catch(err) {
+            return [];
+        }
+        const actions: ViewItemAction[] = [];
+        const items = await container.findElements(By.className('action-label'));
+        
+        for (const item of items) {
+            const label = await item.getAttribute('title');
+            actions.push(await new ViewItemAction(label, this));
+        }
+        return actions;
+    }
+
+    /**
+     * Find action button for view item by label
+     * @param label label of the button to search by
+     * 
+     * @returns ViewItemAction object if such button exists, undefined otherwise
+     */
+    async getActionButton(label: string): Promise<ViewItemAction | undefined> {
+        const actions = await this.getActionButtons();
+        if (actions.length > 0) {
+            return actions.find((item) => { return item.getLabel().indexOf(label) > -1; });
+        } else {
+            return undefined;
+        }
+    }
+}
+
+/**
+ * Action button bound to a view item
+ */
+export class ViewItemAction extends AbstractElement {
+    private label: string;
+
+    constructor(label: string, viewItem: ViewItem) {
+        super(By.xpath(`.//a[contains(@class, 'action-label') and @role='button' and @title='${label}']`), viewItem);
+        this.label = label;
+    }
+
+    /**
+     * Get label of the action button
+     */
+    getLabel(): string {
+        return this.label;
     }
 }
