b0d28b5aa70e114a188eacb408d893c242551989
Jan Richter <jrichter@redhat.com>
2019-08-16 12:30:14 +0200

    context menu tests

---
+++ test/test-project/src/test/menu/contextMenu-test.ts
@@ -0,0 +1,26 @@
+import { expect } from 'chai';
+import { TitleBar, ContextMenu, BottomBarPanel, TitleBarItem } from 'vscode-extension-tester';
+
+describe('ContextMenu', () => {
+    let bar: TitleBar;
+    let menu: ContextMenu;
+
+    beforeEach(async () => {
+        bar = new TitleBar();
+        menu = await bar.select('File') as ContextMenu;
+    });
+
+    afterEach(async () => {
+        await menu.close();
+    });
+
+    it('getItems finds all menu items', async () => {
+        const items = await menu.getItems();
+        expect(items).not.empty;
+    });
+
+    it('getItem finds an item with the given name', async () => {
+        const item = await menu.getItem('New File');
+        expect(item).not.undefined;
+    });
+});
