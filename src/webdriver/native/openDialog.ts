c78a7756d2fdf289c49efc1b28b981bd9e96c731
Jan Richter <jrichter@redhat.com>
2019-08-21 10:57:16 +0200

    Workaround for windows dialog not liking colons

--- src/webdriver/native/openDialog.ts
+++ src/webdriver/native/openDialog.ts
@@ -55,10 +55,15 @@ export class WindowsOpenDialog implements OpenDialog {
         if (!fs.existsSync(absolutePath)) {
             throw new Error('The selected path does not exist');
         }
+        const layout = robot.getKeyboardLayout();
+        layout[':'] = 'shift-semicolon';
+        robot.setKeyboardLayout(layout);
         await robot.sendText(absolutePath);
         await new Promise((res) => { setTimeout(res, 500); });
-        await robot.sendKey('enter');
-        await new Promise((res) => { setTimeout(res, 500); });
+        if (fs.statSync(absolutePath).isDirectory()) {
+            await robot.sendKey('tab');
+            await new Promise((res) => { setTimeout(res, 500); });
+        }
     }
 
     async confirm(): Promise<void> {
