a2d4c02c68156d095b9741c39e0273723885aa79
Jan Richter <jrichter@redhat.com>
2020-05-05 15:00:40 +0200

    Add pre-built locators

---
+++ src/locators/built/1.40.0.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const selenium_webdriver_1 = require("selenium-webdriver");
+exports.diff = {
+    locators: {
+        NotificationsCenter: {
+            close: selenium_webdriver_1.By.className('codicon-close'),
+            clear: selenium_webdriver_1.By.className('codicon-clear-all')
+        }
+    }
+};
+//# sourceMappingURL=1.40.0.js.map
---
+++ src/locators/built/1.41.0.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const selenium_webdriver_1 = require("selenium-webdriver");
+exports.diff = {
+    locators: {
+        ViewSection: {
+            header: selenium_webdriver_1.By.className('pane-header')
+        }
+    }
+};
+//# sourceMappingURL=1.41.0.js.map
