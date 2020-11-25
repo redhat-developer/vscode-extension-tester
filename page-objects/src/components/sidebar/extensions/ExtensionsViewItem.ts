c92bc23846d87f0021cfe1f9f788a7ca501ac47d
Jan Richter <jrichter@redhat.com>
2020-11-25 10:40:33 +0100

    Wait for extension to install

--- page-objects/src/components/sidebar/extensions/ExtensionsViewItem.ts
+++ page-objects/src/components/sidebar/extensions/ExtensionsViewItem.ts
@@ -72,15 +72,23 @@ export class ExtensionsViewItem extends ViewItem {
     }
 
     /**
-     * Install the extension if not installed already
-     * @returns Promise resolving when the Install button is clicked or not found at all
+     * Install the extension if not installed already.
+     * 
+     * Will wait for the extension to finish installing. To skip the wait, set timeout to 0.
+     * 
+     * @param timeout timeout to wait for the installation in milliseconds, default unlimited, set to 0 to skip waiting
+     * @returns Promise resolving when the installation finishes or is skipped
      */
-    async install(): Promise<void> {
+    async install(timeout: number = 300000): Promise<void> {
         if (await this.isInstalled()) {
             return;
         }
         const button = await this.findElement(ExtensionsViewItem.locators.ExtensionsViewItem.install);
+        const manage = await this.findElement(ExtensionsViewItem.locators.ExtensionsViewItem.manage);
         await button.click();
-        await this.getDriver().wait(until.elementIsNotVisible(button));
+
+        if (timeout > 0) {
+            await this.getDriver().wait(until.elementIsVisible(manage), timeout);
+        }
     }
 }
