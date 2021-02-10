233bed1487abfeb7f0cca1f65cb84eebb6d284f6
Jan Richter <jrichter@redhat.com>
2021-02-10 10:47:34 +0100

    Fix simple open dialog

---
+++ test/test-project/src/test/workbench/open-test.ts
@@ -0,0 +1,17 @@
+import { EditorView, InputBox, TitleBar } from 'monaco-page-objects';
+import * as path from 'path';
+
+describe('Simple open file dialog', () => {
+    const filePath = path.resolve('.', 'package.json');
+
+    it('Opens a file', async () => {
+        await new TitleBar().select('File', 'Open File...');
+        const input = await InputBox.create();
+        console.log(filePath.slice(1, filePath.length))
+        await input.setText(filePath);
+        await input.confirm();
+        await new Promise(res => setTimeout(res, 1000));
+
+        await new EditorView().openEditor('package.json')
+    });
+});
