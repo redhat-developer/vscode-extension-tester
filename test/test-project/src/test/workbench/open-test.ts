eff670cb3ebba14f5120806cac249ec457beb645
Jan Richter <jrichter@redhat.com>
2021-02-10 10:58:58 +0100

    make test runnable on mac

--- test/test-project/src/test/workbench/open-test.ts
+++ test/test-project/src/test/workbench/open-test.ts
@@ -1,11 +1,11 @@
-import { EditorView, InputBox, TitleBar } from 'monaco-page-objects';
+import { EditorView, InputBox, Workbench } from 'monaco-page-objects';
 import * as path from 'path';
 
 describe('Simple open file dialog', () => {
     const filePath = path.resolve('.', 'package.json');
 
     it('Opens a file', async () => {
-        await new TitleBar().select('File', 'Open File...');
+        await new Workbench().executeCommand('file: open file');
         const input = await InputBox.create();
         console.log(filePath.slice(1, filePath.length))
         await input.setText(filePath);
