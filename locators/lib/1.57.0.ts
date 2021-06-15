9c339dc9df95d415b64834bcc4860be6e0e02385
Jan Richter <jrichter@redhat.com>
2021-06-15 13:55:40 +0200

    Disable workspace trust

---
+++ locators/lib/1.57.0.ts
@@ -0,0 +1,11 @@
+import { LocatorDiff } from "monaco-page-objects";
+import { By } from "selenium-webdriver";
+
+export const diff: LocatorDiff = {
+    locators: {
+        EditorView: {
+            settingsEditor: By.className('settings-editor'),
+            webView: By.xpath(`.//div[starts-with(@id, 'webview-editor')]`)
+        }
+    }
+}
