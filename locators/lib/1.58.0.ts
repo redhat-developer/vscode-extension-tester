272c934ece30485070e52aa3d63bdf7b406091c4
Jan Richter <jrichter@redhat.com>
2021-07-12 12:18:26 +0200

    Fix statusbar getItem for new vscode

---
+++ locators/lib/1.58.0.ts
@@ -0,0 +1,9 @@
+import { LocatorDiff } from "monaco-page-objects";
+
+export const diff: LocatorDiff = {
+    locators: {
+        StatusBar: {
+            itemTitle: 'aria-label'
+        }
+    }
+}
