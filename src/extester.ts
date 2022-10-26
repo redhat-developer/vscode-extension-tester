2d079833d48fa793dd5c26b547ea920ad5b734a2
Dominik Jelinek <djelinek@redhat.com>
2022-10-26 20:09:43 +0200

    Bump max placeholder to 1.72.2
    Signed-off-by: Dominik Jelinek <djelinek@redhat.com>

--- src/extester.ts
+++ src/extester.ts
@@ -27,7 +27,7 @@ export const DEFAULT_SETUP_OPTIONS = {
 }
 
 export const VSCODE_VERSION_MIN = '1.67.2';
-export const VSCODE_VERSION_MAX = '1.71.2';
+export const VSCODE_VERSION_MAX = '1.72.2';
 
 /**
  * VSCode Extension Tester
