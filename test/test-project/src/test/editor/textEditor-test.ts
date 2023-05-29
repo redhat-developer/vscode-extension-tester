14e0d0a7557de02fe0adc19353920dc610656cf7
Dominik Jelinek <djelinek@redhat.com>
2023-05-29 12:12:42 +0200

    issue-795: TextEditor > typeTextAt() method is not properly working mainly on macOS
    Signed-off-by: Dominik Jelinek <djelinek@redhat.com>

--- test/test-project/src/test/editor/textEditor-test.ts
+++ test/test-project/src/test/editor/textEditor-test.ts
@@ -110,19 +110,14 @@ describe('TextEditor', function () {
 
     it('getCoordinates works', async function () {
         this.timeout(15000);
-        await editor.moveCursor(1, 1);
 
+        await editor.moveCursor(1, 1);
         expect(await editor.getCoordinates()).to.deep.equal([1, 1]);
 
         const lineCount = await editor.getNumberOfLines();
         const lastLine = await editor.getTextAtLine(lineCount);
 
         await editor.moveCursor(lineCount, lastLine.length);
-        await editor.getDriver().wait(async function () {
-            const coor = await editor.getCoordinates();
-            return coor[0] === lineCount && coor[1] === lastLine.length;
-        }, this.timeout() - 3000, `Cursor move did not finished properly!`);
-
         expect(await editor.getCoordinates()).to.deep.equal([lineCount, lastLine.length]);
     });
 
