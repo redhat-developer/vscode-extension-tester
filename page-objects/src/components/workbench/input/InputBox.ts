bad509993a055857bce5e4070955c0b3a7393c92
infiniteregrets <aroram18@mcmaster.ca>
2023-06-02 14:16:48 -0400

    Add documentation

--- page-objects/src/components/workbench/input/InputBox.ts
+++ page-objects/src/components/workbench/input/InputBox.ts
@@ -12,6 +12,7 @@ export class InputBox extends Input {
     /**
      * Construct a new InputBox instance after waiting for its underlying element to exist
      * Use when an input box is scheduled to appear.
+     * @param timeout max time to wait in milliseconds, default 5000
      */
     static async create(timeout: number = 5000): Promise<InputBox> {
         await InputBox.driver.wait(until.elementLocated(InputBox.locators.InputBox.constructor), timeout);
