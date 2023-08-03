d843efedac416e8e0cb5fea519a6608fa0e7e6e0
Dominik Jelinek <djelinek@redhat.com>
2023-08-04 00:41:52 +0200

    Bump 'max' placeholder to latest vscode 1.81.0
    Signed-off-by: Dominik Jelinek <djelinek@redhat.com>

--- test/test-project/src/test/bottomBar/bottomBarPanel-test.ts
+++ test/test-project/src/test/bottomBar/bottomBarPanel-test.ts
@@ -1,31 +1,32 @@
 import { expect } from 'chai';
 import { BottomBarPanel, WebElement, Workbench, ViewControl, ActivityBar } from 'vscode-extension-tester';
 
-describe('BottomBarPanel', () => {
+describe('BottomBarPanel', function () {
     let panel: BottomBarPanel;
 
-    before(async () => {
+    before(async function () {
         panel = new BottomBarPanel();
         await (await new Workbench().openNotificationsCenter()).clearAllNotifications();
         await (await new ActivityBar().getViewControl('Explorer') as ViewControl).openView();
     });
 
-    after(async () => {
+    after(async function () {
         await panel.toggle(false);
     });
 
-    it('can be toggled open', async () => {
+    it('can be toggled open', async function () {
         await panel.toggle(true);
         expect(await panel.isDisplayed()).is.true;
     });
 
-    it('can be toggled closed', async () => {
+    it('can be toggled closed', async function () {
         await panel.toggle(true);
         await panel.toggle(false);
         expect(await panel.isDisplayed()).is.false;
     });
 
-    it('can be maximized and restored', async () => {
+    it('can be maximized and restored', async function () {
+        this.timeout(20000);
         await panel.toggle(true);
         const initHeight = await getHeight(panel);
 
@@ -39,22 +40,22 @@ describe('BottomBarPanel', () => {
         expect(initHeight).equals(restoredHeight);
     });
 
-    it('can open problems view', async () => {
+    it('can open problems view', async function () {
         const view = await panel.openProblemsView();
         expect(await view.isDisplayed()).is.true;
     });
 
-    it('can open output view', async () => {
+    it('can open output view', async function () {
         const view = await panel.openOutputView();
         expect(await view.isDisplayed()).is.true;
     });
 
-    it('can open debug console view', async () => {
+    it('can open debug console view', async function () {
         const view = await panel.openDebugConsoleView();
         expect(await view.isDisplayed()).is.true;
     });
 
-    it('can open terminal view', async () => {
+    it('can open terminal view', async function () {
         const view = await panel.openTerminalView();
         expect(await view.isDisplayed()).is.true;
     });
