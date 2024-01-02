468be2b4723b60ee7fe0740d862c3fdc39592ecf
Dominik Jelinek <djelinek@redhat.com>
2024-01-02 23:22:10 +0100

    get back to targz.decompress to fix linux
    Signed-off-by: Dominik Jelinek <djelinek@redhat.com>

--- src/util/unpack.ts
+++ src/util/unpack.ts
@@ -1,17 +1,23 @@
 import * as fs from 'fs-extra';
 import { exec } from 'child_process';
-const targz = require('tar');
+const targz = require('targz');
 import * as unzipper from 'unzipper';
 
 export class Unpack {
     static unpack(input: fs.PathLike, target: fs.PathLike): Promise<void> {
         return new Promise((resolve, reject) => {
             if (input.toString().endsWith('.tar.gz')) {
-                targz.extract({
-                    file: input,
-                    sync: true,
-                    cwd: target
+                targz.decompress({
+                    src: input,
+                    dest: target
+                }, (err: Error) => {
+                    err ? reject(err) : resolve();
                 });
+                // targz.extract({
+                //     file: input,
+                //     sync: true,
+                //     cwd: target
+                // });
             }
             else if (input.toString().endsWith('.zip')) {
                 if (process.platform === 'darwin') {
