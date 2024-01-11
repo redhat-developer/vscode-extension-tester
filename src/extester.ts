542c3ff38fb0e002eef9c02b200698291dfd9f36
Wesley Pickering <wesley.pickering@c3.ai>
2024-01-11 14:32:49 -0800

    issue-1071: Fix downloading chromedriver version > 114
    Signed-off-by: Dominik Jelinek <djelinek@redhat.com>
    
    Based on initial work of Wesley pickering <wesley.pickering@c3.ai>

--- src/extester.ts
+++ src/extester.ts
@@ -128,7 +128,7 @@ export class ExTester {
         } else {
             console.log('Attempting Setup in offline mode');
             const expectedChromeVersion = (await this.code.checkOfflineRequirements()).split('.')[0];
-            const actualChromeVersion = (await this.chrome.checkDriverVersionOffline()).split('.')[0];
+            const actualChromeVersion = (await this.chrome.checkDriverVersionOffline(vscodeParsedVersion)).split('.')[0];
             if (expectedChromeVersion !== actualChromeVersion) {
                 console.log('\x1b[33m%s\x1b[0m', `WARNING: Local copy of VS Code runs Chromium version ${expectedChromeVersion}, the installed ChromeDriver is version ${actualChromeVersion}.`)
                 console.log(`Attempting with ChromeDriver ${actualChromeVersion} anyway. Tests may experience issues due to version mismatch.`);
