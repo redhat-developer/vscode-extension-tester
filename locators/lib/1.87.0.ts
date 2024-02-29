b7d309f877d2e1f203e6da6779f6223847232e45
Dominik Jelinek <djelinek@redhat.com>
2024-02-29 14:35:01 +0100

    test: Fix BottomBarPanel tests for 1.87.0
    Signed-off-by: Dominik Jelinek <djelinek@redhat.com>

---
+++ locators/lib/1.87.0.ts
@@ -0,0 +1,10 @@
+import { By, LocatorDiff } from "monaco-page-objects";
+export const diff: LocatorDiff = {
+    locators: {
+        BottomBarPanel: {
+            close: 'Hide Panel',
+            globalActions: By.className('global-actions'),
+            action: (label: string) => By.xpath(`.//a[starts-with(@aria-label, '${label}')]`)
+        }
+    }
+}
