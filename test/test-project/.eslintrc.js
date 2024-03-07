413f4bff643bf04c2f66ee109868285f0cec0631
Dominik Jelínek <djelinek@redhat.com>
2024-03-07 15:47:17 +0100

    fix: Improve TSconfig setup and fix errors in test/test-project (#1177)
    Signed-off-by: Dominik Jelinek <djelinek@redhat.com>

--- test/test-project/.eslintrc.js
+++ test/test-project/.eslintrc.js
@@ -1,12 +1,6 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
-    },
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
-        "project": "tsconfig.json",
         "sourceType": "module"
     },
     "plugins": [
