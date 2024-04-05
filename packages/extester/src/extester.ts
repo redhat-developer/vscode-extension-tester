acfd3ac814f713e3ec0c5568e1f592549e6e696c
Dominik Jelinek <djelinek@redhat.com>
2024-04-05 10:16:30 +0200

    build: bump max placeholder from 1.87.2 to 1.88.0
    Signed-off-by: Dominik Jelinek <djelinek@redhat.com>

--- packages/extester/src/extester.ts
+++ packages/extester/src/extester.ts
@@ -29,8 +29,8 @@ export const DEFAULT_SETUP_OPTIONS = {
 
 export const DEFAULT_STORAGE_FOLDER = process.env.TEST_RESOURCES ? process.env.TEST_RESOURCES : path.join(os.tmpdir(), 'test-resources');
 
-export const VSCODE_VERSION_MIN = '1.85.2';
-export const VSCODE_VERSION_MAX = '1.87.2';
+export const VSCODE_VERSION_MIN = '1.86.2';
+export const VSCODE_VERSION_MAX = '1.88.0';
 
 /**
  * The latest version with automated tests
