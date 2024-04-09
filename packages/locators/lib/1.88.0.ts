863929ab7e422c8799f5003610cdeb0d170c589e
Dominik Jelinek <djelinek@redhat.com>
2024-04-09 12:13:48 +0200

    test: Fix textEditor.test.ts for VS Code 1.88.x
    Signed-off-by: Dominik Jelinek <djelinek@redhat.com>

---
+++ packages/locators/lib/1.88.0.ts
@@ -0,0 +1,10 @@
+import { By, LocatorDiff } from "@redhat-developer/page-objects";
+
+export const diff: LocatorDiff = {
+    locators: {
+        FindWidget: {
+            toggleReplace: By.xpath(`.//div[@aria-label="Toggle Replace"]`),
+            button: (title: string) => By.xpath(`.//div[@role='button' and starts-with(@aria-label, "${title}")]`)
+        }
+    }
+}
