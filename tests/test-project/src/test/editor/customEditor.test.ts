3bc78ca68748e7eed6c62876efb3a1529f43921c
Dominik Jelinek <djelinek@redhat.com>
2024-10-02 16:09:31 +0200

    feat: improve static sleep during openResources method
    Signed-off-by: Dominik Jelinek <djelinek@redhat.com>

--- tests/test-project/src/test/editor/customEditor.test.ts
+++ tests/test-project/src/test/editor/customEditor.test.ts
@@ -25,7 +25,7 @@ describe('CustomEditor', () => {
 	const CUSTOM_TITLE: string = 'example.cscratch';
 
 	before(async () => {
-		await VSBrowser.instance.openResources(path.resolve(__dirname, '..', '..', '..', 'resources', CUSTOM_TITLE));
+		await VSBrowser.instance.openResources({ path: path.resolve(__dirname, '..', '..', '..', 'resources', CUSTOM_TITLE) });
 		editor = new CustomEditor();
 	});
 
