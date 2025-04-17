4379af902982b4607584c4ff0d08500449824c33
Dominik Jelinek <djelinek@redhat.com>
2025-04-17 23:29:56 +0200

    test: disable eslint warnings for unit test files

--- packages/extester-runner/src/test/suite/parser.test.ts
+++ packages/extester-runner/src/test/suite/parser.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /**
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
