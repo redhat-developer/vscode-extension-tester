8ee29d78fa862fcd8f387243f1cafc8938111298
Michael Pollind <mpollind@qti.qualcomm.com>
2025-04-24 10:52:52 -0700

    feat: allow download to disable tls check env flag
    Signed-off-by: Michael Pollind <mpollind@qti.qualcomm.com>

--- packages/extester/src/util/download.ts
+++ packages/extester/src/util/download.ts
@@ -26,6 +26,7 @@ const httpProxyAgent = !process.env.HTTP_PROXY
 			proxy: process.env.HTTP_PROXY,
 		});
 
+const rejectUnauthorized = +(process.env.HTTPS_TLS_REJECT_UNAUTHORIZED ?? 1) >= 1;
 const httpsProxyAgent = !process.env.HTTPS_PROXY
 	? undefined
 	: new HttpsProxyAgent({
@@ -33,6 +34,9 @@ const httpsProxyAgent = !process.env.HTTPS_PROXY
 		});
 
 const options = {
+	https: {
+		rejectUnauthorized,
+	},
 	headers: {
 		'user-agent': 'nodejs',
 	},
