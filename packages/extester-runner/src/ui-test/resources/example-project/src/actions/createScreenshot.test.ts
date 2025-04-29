b159b0648c711f58dfbc4a6d0cecdfd63e35fc1f
Filip Pospisil <fpospisi@redhat.com>
2025-04-30 00:05:39 +0200

    ui-test: init test for screenshots view

--- packages/extester-runner/src/ui-test/resources/example-project/src/actions/createScreenshot.test.ts
+++ packages/extester-runner/src/ui-test/resources/example-project/src/actions/createScreenshot.test.ts
@@ -5,6 +5,7 @@ describe('Create Screenshot', function () {
 	this.timeout(15000);
 	it('Trigger takeScreenshot action', async function () {
 		await VSBrowser.instance.takeScreenshot('screenshot1');
+		await VSBrowser.instance.takeScreenshot('screenshot2');
 		assert.isTrue(true);
 	});
 });
