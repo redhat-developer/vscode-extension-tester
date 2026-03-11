65a55e931844051e1df3cdc23e1975dd45b1c117
Dominik Jelínek <djelinek@redhat.com>
2026-03-11 20:27:03 +0100

    feat: support for VS Code 1.111.x (#2300)

--- packages/locators/lib/1.110.0.ts
+++ packages/locators/lib/1.110.0.ts
@@ -15,15 +15,13 @@
  * limitations under the License.
  */
 
-import { By, LocatorDiff } from '@redhat-developer/page-objects';
+import { LocatorDiff } from '@redhat-developer/page-objects';
 
 export const diff: LocatorDiff = {
 	locators: {
-		Workbench: {
-			notificationContainer: By.className('notifications-list-container'),
-		},
-		ViewTitlePart: {
-			actionConstructor: (title: string) => By.xpath(`.//a[@aria-label='${title}']`),
+		BottomBarPanel: {
+			maximize: 'Maximize Panel',
+			restore: 'Restore Panel',
 		},
 	},
 };
