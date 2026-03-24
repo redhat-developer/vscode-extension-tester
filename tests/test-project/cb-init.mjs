6168b6235bf83c9863480d8894b5f891fea0ca76
Florian Lefebvre <contact@florian-lefebvre.dev>
2026-03-24 09:44:54 +0100

    feedback

--- tests/test-project/cb-init.mjs
+++ tests/test-project/cb-init.mjs
@@ -2,7 +2,7 @@ import { writeText } from 'tinyclip';
 
 /** @returns {Promise<string>} */
 async function getInput() {
-	return new Promise(async (resolve) => {
+	return new Promise((resolve) => {
 		process.stdin.on('data', (chunk) => resolve(chunk.toString().trim()));
 	});
 }
