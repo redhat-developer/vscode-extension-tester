480af2d0cdb1caa42f28e75b3672e559a312ad03
djelinek <18696866+djelinek@users.noreply.github.com>
2026-08-24 09:20:03 +0000

    chore: update supported VS Code versions
    - vscode-min: 1.129.1 → 1.132.1
    - vscode-max: 1.131.0 → 1.134.0

--- packages/extester/src/util/download.ts
+++ packages/extester/src/util/download.ts
@@ -50,8 +50,8 @@ const options = {
 		https: httpsProxyAgent,
 	},
 	timeout: {
-		request: 180_000,
-		response: 120_000,
+		request: 300_000, // 5 minutes
+		response: 120_000, // 2 minutes
 	},
 	retry: {
 		limit: retryCount,
